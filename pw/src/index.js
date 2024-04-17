0b9c04c9943df98e44119ea170bfcd91669d9312
Dustin <dustin.baum@miele.com>
2024-04-17 08:48:51 +0200

    passwords from top10.000 list

--- pw/src/index.js
+++ pw/src/index.js
@@ -1,21 +1,15 @@
-const {Dictionary} = require("./Dictionary");
+const { WordList } = require("./WordList");
 const {Generator, Capitalisation} = require("./Generator");
 
-let gerDict = new Dictionary("German_de_DE", 8);
-let engDict = new Dictionary("English (American)", 8);
+let deList = new WordList("top10000de.txt", 8);
+let enList = new WordList("top10000en.txt", 8);
 
-let generator = new Generator(gerDict, engDict);
+let generator = new Generator(deList, enList);
 generator.capitalisation = Capitalisation.CAMEL_CASE;
 generator.wordCount = 4;
 
-const keySpace = Math.pow(generator.dictionaryLength, generator.wordCount);
-const crackingSpeed = Math.pow(10, 12); // 1 billion / sec
-console.debug(generator.dictionaryLength, "dictionary entries ^", generator.wordCount, "words =", keySpace.toExponential(3), "possibilities");
-const crackingDurationYears = Math.round(keySpace / crackingSpeed / 60 / 60 / 24 / 365 * 100) / 200; // additional division by 2: average seached keyspace
-console.debug("brute force duration:", crackingDurationYears, "years @", crackingSpeed.toExponential(), "tries per second")
-
-const passwordCount = 40;
-console.debug("generating", passwordCount, "passwords:\n");
+console.debug("");
+const passwordCount = 100;
 for (var i = 0; i < passwordCount; i++) {
-    console.log(i, generator.generatePassword());
+    console.log(i + 1, generator.generatePassword());
 }
